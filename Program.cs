afaa55e8f554ba26954678acfa4018a0776e01e6
ivailojekov <56577780+ivailojekov@users.noreply.github.com>
2019-10-16 16:05:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace danceHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hallLength = double.Parse(Console.ReadLine());
+            double hallWidth = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+
+            double wardrobeArea = (wardrobeSide * 100) * (wardrobeSide * 100);
+            double hallAreaM = (hallLength * 100) * (hallWidth * 100);
+            double seatArea = hallAreaM / 10;
+            double cleanArea = hallAreaM - seatArea - wardrobeArea;
+            double dancersNumber = cleanArea / (40 + 7000);
+
+            Console.WriteLine(Math.Floor(dancersNumber));
+           
+        }
+    }
+}
